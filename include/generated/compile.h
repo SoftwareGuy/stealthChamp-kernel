e6149b7ec9a83e2450fb8ff52df62d681b5126e4
Matt Coburn <coburn@dandere.net>
2021-01-27 14:30:57 +1000

    add all kernel files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Tue Jan 12 23:06:09 AEST 2021"
+#define LINUX_COMPILE_BY "coburn"
+#define LINUX_COMPILE_HOST "musashi"
+#define LINUX_COMPILER "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "5.9.10-stealthChamp"
